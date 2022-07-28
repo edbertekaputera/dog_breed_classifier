ae57d6e10385371d1b594e0666e348ec3bd3f3d7
Edbert Ekaputera <edbertekaputera@gmail.com>
2022-07-28 17:51:13 +0800

    new commit

--- main.py
+++ main.py
@@ -3,15 +3,103 @@ import tensorflow as tf
 import tensorflow_hub as hub
 import numpy as np
 import os
-from func import load_model, dog_breed_predict
+import cv2
+from object import ObjectDetectModel
+
+class DogModel(ObjectDetectModel):
+   def __init__(self, model_path) -> None:
+      super().__init__()
+      # Declaring instances
+      self.breed_model = None
+   
+      # Loads model
+      self.load_model(model_path)
+
+   def load_model(self, model_path):
+      """
+      Loads a saved model from a specified path
+      """
+      print(f"Loading saved model from: {model_path}")
+      model = tf.keras.models.load_model(model_path,
+                                          custom_objects={"KerasLayer" : hub.KerasLayer})
+      self.breed_model = model
+   
+   def process_image(self, image, length=224, width=224):
+      """
+      Processes the image into a 224x224x3 tensor with values ranging from 0-1, 
+      then converts it to a batch for prediction
+      """
+      new_image, coordinates = self.getDogCrop(image)
+      if coordinates == -1:
+         return -1, -1
+      processed_image = tf.image.convert_image_dtype(new_image, tf.float32)
+      #Resize to (224,224)
+      processed_image = tf.image.resize(processed_image, size=[length, width])
+      return tf.data.Dataset.from_tensor_slices([processed_image]).batch(32), coordinates
+      
+   # Check accuracy of prediction
+   def predict(self, image, check=False):
+      processed_image, coordinate = self.process_image(image)
+      if processed_image == -1:
+         return -1, -1
+      self.prediction_probability = self.breed_model.predict(processed_image)
+      if check:
+         breed = self.predict_check()
+         return coordinate, breed
+      else:
+         return coordinate
+      
+   def predict_check(self, verbose=False):
+      labels = []
+      for folders in os.listdir("Images/train_images"):
+         labels.append("".join(folders.split("-")[1:]))
+      unique_breeds = np.unique(labels)
+      if verbose:
+         print(unique_breeds)
+         print(self.prediction_probability)
+      label = unique_breeds[np.argmax(self.prediction_probability[0])]
+      score = np.max(self.prediction_probability[0])
+      print(f"Max value (probability of prediction) : {score}")
+      print(f"Sum : {np.sum(self.prediction_probability[0])}")
+      print(f"Max Index: {np.argmax(self.prediction_probability[0])}")
+      print(f"Predicted label: {label}")
+      return (label, score)
 
 def main():
-   model = load_model("Models/2022_06_15-16_001655308824-full-image-set-mobilenetv2-Adam.h5")
-   while(True):
-      image = [input("Enter the image path: ")]
-      dog_breed_predict(image, model)
-      if (input("Try again? (y/n) ").lower() == "n"):
-         print("QUITTING...")
+   model_path = "full_model.hdf5"
+   my_model = DogModel(model_path)
+   video = cv2.VideoCapture(1)
+   counter = 0
+   RED = (0,0,255)
+   WHITE = (255, 255, 255)
+   box_list = []
+   text_list = []
+   while True:
+      ret, frame = video.read()
+      if counter == 60:
+         box_list = []
+         text_list = []
+         coordinates, breed = my_model.predict(frame, check=True)
+         if coordinates != -1:
+            box_list.append(coordinates)
+            text_list.append(f"DOGBREED = {breed[0]} {breed[1]*100:.2f}%")
+         counter = 0
+      counter+= 1   
+
+      # Draws all bounding boxes
+      for i in range(len(box_list)):
+         pos1 = box_list[i][0]
+         pos2 = box_list[i][1]
+         pos_text = (pos1[0]+12, pos1[1]+12)
+         pos_text_rect_1 = (pos1[0], pos1[1])
+         pos_text_rect_2 = (pos2[0], pos1[1]+15)
+         cv2.rectangle(frame, pos1, pos2, RED, 4)
+         cv2.rectangle(frame, pos_text_rect_1, pos_text_rect_2, RED, -1)
+         cv2.putText(frame,  text_list[0], pos_text, cv2.FONT_HERSHEY_DUPLEX, 0.5, WHITE)
+      cv2.imshow("test", frame)
+      
+      #Waits for a user input to quit the application
+      if cv2.waitKey(1) & 0xFF == ord('q'):
          break
 
 if __name__ == "__main__":   
---
+++ object.py
@@ -0,0 +1,136 @@
+# Imported Libraries
+from cv2 import FONT_HERSHEY_DUPLEX, FONT_HERSHEY_PLAIN
+import tensorflow_hub as hub
+import tensorflow as tf
+import numpy as np
+import cv2
+from time import time
+import matplotlib.pyplot as plt
+# from main import DogModel
+
+# ObjectDetectionModel Class
+class ObjectDetectModel:
+   def __init__(self) -> None:
+      self.OBJECT_MODEL_PATH = "ssd_mobilenet_v2_2"
+      self.object_model = tf.saved_model.load(self.OBJECT_MODEL_PATH)
+
+   def process_whole_image(self, image):
+      """
+      Processes the image into a RGB tensor with values ranging from 0-1, 
+      Parameters:
+      image = Image to be processed;
+      """
+      processed_image = tf.image.convert_image_dtype(image, tf.uint8)
+      return np.array([processed_image])
+
+   def detect_object(self, image):
+      """
+      This function processes the image with the `process_whole_image` function,
+      then detects the objects within the processed image using SSD-MobileNet V2 model,
+      and returns (class_id, boxes, scores).
+      -> class_ids: tf.int tensor of shape [N] containing detection class index from the label file.
+      -> boxes: tf.float32 tensor of shape [N, 4] containing bounding box coordinates in the following order: [ymin, xmin, ymax, xmax].
+      -> scores: a tf.float32 tensor of shape [N] containing detection scores.
+      
+      Parameters:
+      image: Image to be detected.
+      """
+      img = self.process_whole_image(image)
+      results = self.object_model(img)
+      results = {key:value.numpy() for key,value in results.items()}
+      class_ids = results["detection_classes"][0]
+      boxes = results["detection_boxes"][0]
+      scores = results["detection_scores"][0]
+      return class_ids, boxes, scores
+
+   def getDog(self, image):
+      """
+      This function uses the `process_whole_image` and `detect` to process and detect dogs in the image,
+      then returns (detection_score, coordinates). If no dogs are found, returns (-1, -1).
+      detection_score = confidence score of the detection.
+      coordinates = two tuples (xmin, ymin), (xmax, ymax) representing both corner coordinates of the bounding box.
+      
+      Parameters:
+      image = Image to be detected.
+      """
+      im_height, im_width = image.shape[:2]
+      class_ids, boxes, scores = self.detect_object(image)
+
+      for i in range(min(boxes.shape[0], 10)):
+         if scores[i] >= 0.1 and class_ids[i] == 18:
+            xmin = int(boxes[i][1] * im_width) - 10 if int(boxes[i][1] * im_width) - 10 > 0 else 0
+            xmax = int(boxes[i][3] * im_width) + 10 if int(boxes[i][3] * im_width) + 10 < im_width else im_width
+            ymin = int(boxes[i][0] * im_height) - 10 if int(boxes[i][0] * im_height) - 10 > 0 else 0
+            ymax = int(boxes[i][2] * im_height) + 10 if int(boxes[i][2] * im_height) + 10 < im_height else im_height
+            coordinates = ((xmin, ymin), (xmax, ymax))
+            return scores[i], coordinates
+      return -1, -1
+
+   def getDogCrop(self, image):
+      scores, coordinates = self.getDog(image)
+      newimg = -1
+      if scores != -1:
+         xmin = coordinates[0][0]
+         xmax = coordinates[1][0]
+         ymin = coordinates[0][1]
+         ymax = coordinates[1][1]
+         newimg = image[ymin:ymax, xmin:xmax]
+         return newimg, ((xmin, ymin), (xmax, ymax))
+      else:
+         return -1, -1
+
+def main():
+   detect_model = ObjectDetectModel()
+   # image = tf.io.read_file("test.png")
+   # image = tf.io.decode_png(image, channels=3)
+   # plt.imshow(image)
+   # img = detect_model.process_whole_image(image)
+   
+   # for i in range(len(scores)):
+   #    if scores[i] >= 0.1:
+   #       print(f"class = {class_ids[i]}, boxes = {boxes[i]}, scores = {(scores[i]*100):.2f}%")
+
+   video = cv2.VideoCapture(1)
+   # cv2.imshow("test", draw)
+   counter = 0
+   cropcount = 1
+   RED = (0,0,255)
+   WHITE = (255, 255, 255)
+   box_list = []
+   text_list = []
+   while True:
+      ret, frame = video.read()
+      if counter == 60:
+         box_list = []
+         text_list = []
+         scores, coordinates = detect_model.getDog(frame)
+         if scores != -1:
+            xmin = coordinates[0][0]
+            xmax = coordinates[1][0]
+            ymin = coordinates[0][1]
+            ymax = coordinates[1][1]
+            new_img = frame[ymin:ymax, xmin:xmax]
+            cv2.imwrite(f"crop{cropcount}.png", new_img)
+            cropcount+=1
+
+            box_list.append(coordinates)
+            text_list.append(f"DOG {scores*100:.2f}%")
+         counter = 0
+      counter+= 1
+      #Waits for a user input to quit the application
+      if len(box_list) > 0:
+         pos1 = box_list[0][0]
+         pos2 = box_list[0][1]
+         pos_text = (pos1[0]+12, pos1[1]+12)
+         pos_text_rect_1 = (pos1[0], pos1[1])
+         pos_text_rect_2 = (pos2[0], pos1[1]+15)
+         cv2.rectangle(frame, pos1, pos2, RED, 4)
+         cv2.rectangle(frame, pos_text_rect_1, pos_text_rect_2, RED, -1)
+         cv2.putText(frame,  text_list[0], pos_text, FONT_HERSHEY_DUPLEX, 0.5, WHITE)
+      cv2.imshow("test", frame)
+      if cv2.waitKey(1) & 0xFF == ord('q'):
+         break
+
+
+if __name__ == "__main__":
+   main()
